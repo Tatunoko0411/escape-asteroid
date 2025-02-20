a96dce268fdcf5e42f01c9279d359a4750f24962
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-02-20 11:11:13 +0900

    個人のシーンを作成

---
+++ Assets/Script/Dice.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public int Dice1Max;
+    public int Dice2Max;
+    public int Dice1Min;
+    public int Dice2Min;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DiceRoll()
+    {
+
+    }
+
+
+}
