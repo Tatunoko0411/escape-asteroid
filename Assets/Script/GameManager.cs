146710abae2faad59d7ff6ce374daf49ae0a8911
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-02-21 13:55:52 +0900

    アイテムの取得を追加しました（アイテムの詳細は未実装）

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] public List<GameObject> trouts;
-
+    [SerializeField]  public GameObject DiceRollButton; 
+    [SerializeField] public GameObject BackButton;
+    [SerializeField] public GameObject GetItemUI;
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    } 
 
     // Update is called once per frame
     void Update()
@@ -22,5 +25,19 @@ public class GameManager : MonoBehaviour
     public void ChangeDirection()
     {
         GameObject.Find($"Player").GetComponent<Player>().direction = - GameObject.Find($"Player").GetComponent<Player>().direction ;
+    }
+
+    public void GetItem(bool Get)
+    {
+        if (Get)
+        {
+            //はいを選択した場合
+            GameObject.Find($"Player").GetComponent<Player>().items.Add(new Item() {Tire = 0,ID = 1});
+            Debug.Log("アイテムを手に入れた!");
+        }
+
+        DiceRollButton.SetActive(true);
+        BackButton.SetActive(true);
+        GetItemUI.SetActive(false);
     }
 }
