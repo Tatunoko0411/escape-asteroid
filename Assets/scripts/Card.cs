bb30a1ee126d431d86c641fcb6601e6094671aea
Wata0912 <241211@st.yoshida-g.ac.jp>
2025-03-04 11:49:30 +0900

    カード使用処理の追加

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card 
+{
+    public int id;
+    public string name;
+    
+
+    public int ID
+    {
+        set { id  = value; }
+        get { return id; }
+    }
+
+    public string NAME
+    {
+        set { name = value; }
+        get { return name; }
+    }
+
+   
+
+
+    public Card()
+    {
+
+    }
+
+
+}
